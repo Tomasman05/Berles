9d0f36272db47bc91fb0a635897b9bc7bef4925d
NemethAngela <a.nemet75h@yahoo.com>
2023-02-17 09:09:17 +0100

    ongoing

---
+++ src/app.js
@@ -0,0 +1,5 @@
+const firstCheck = document.querySelector("#owner");
+const secondCheck = document.querySelector("#cusRenter");
+firstCheck.addEventListener("click", () => {
+    secondCheck="click";
+})
