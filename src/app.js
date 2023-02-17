fbfbfb6db1b29ccf079b4fce9aec5902d7f7242f
NemethAngela <a.nemet75h@yahoo.com>
2023-02-17 10:45:22 +0100

    ongoing

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
-const firstCheck = document.querySelector("#owner");
-const secondCheck = document.querySelector("#cusRenter");
-firstCheck.addEventListener("click", () => {
-    secondCheck="click";
+const ownerRadio=document.querySelector("#owner")
+const renterRadio=document.querySelector("#renter")
+const cusRenterRadio=document.querySelector("#cusRenter")
+const subCusRenterRadio=document.querySelector("#subCusRenter")
+
+ownerRadio.addEventListener("click", ()=>{
+    console.log("Az eladó egy tulajdonos")
+    cusRenterRadio.checked=true
+})
+renterRadio.addEventListener("click", ()=>{
+    console.log("Az eladó eleve bérlő")
+    subCusRenterRadio.checked = true
 })
